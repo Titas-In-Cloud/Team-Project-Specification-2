7f18cb100e3ac91829550ecfa4aaf36dd385d343
Jay Mavin <b9034710@newcastle.ac.uk>
2019-11-02 16:22:09 +0000

    Making a blog

--- specification-3/web.py
+++ specification-3/web.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+posts =  [
+    {
+        'author': 'Jay Mavin',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 2, 2019'
+    },
+    {
+        'author': 'James Smith',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 3, 2019'
+    }
+]
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
