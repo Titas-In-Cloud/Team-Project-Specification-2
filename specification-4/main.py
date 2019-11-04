a090ee679359b88db05133b250cb8eacf7983cb5
Wojciech Bigosiński <b9024302@newcastle.ac.uk>
2019-11-04 17:44:15 +0000

    Added enemy

--- specification-4/main.py
+++ specification-4/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import (
    K_UP,
    K_DOWN,
@@ -36,6 +37,24 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = screen_height
 
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(screen_width + 20, screen_width + 100),
+                random.randint(0, screen_height),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 
 pygame.init()
 screen_width = 800
