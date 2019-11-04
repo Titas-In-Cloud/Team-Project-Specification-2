713bbbda3616bb8af5dd947caf0142cd95667d40
Wojciech Bigosiński <b9024302@newcastle.ac.uk>
2019-11-04 17:11:49 +0000

    Added player movement - it's very fast tho

--- specification-4/main.py
+++ specification-4/main.py
@@ -9,40 +9,56 @@ from pygame.locals import (
    QUIT,
 )
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((0, 0, 0))
+        self.rect = self.surf.get_rect()
+
+    def update1(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+
 
 pygame.init()
-screen_width = 1920
-screen_height = 1080
+screen_width = 800
+screen_height = 600
+
 
 # Set up the drawing window
 screen = pygame.display.set_mode([screen_width, screen_height])
 
+player = Player()
 # Run until the user asks to quit
 running = True
 while running:
 
    # Did the user click the window close button?
-   for event in pygame.event.get():
+    for event in pygame.event.get():
        if event.type == KEYDOWN:
            if event.key == K_ESCAPE:
                running = False
        elif event.type == QUIT:
            running = False
 
-   # Fill the background with white
-   screen.fill((255, 255, 255))
+    pressed_keys = pygame.key.get_pressed()
 
-   surf = pygame.Surface((1500, 1000))
-   surf.fill((0, 0, 0))
-   rect = surf.get_rect()
-   screen.blit(surf, (0, 0))
+    player.update1(pressed_keys)
+    # Fill the background with white
+    screen.fill((255, 255, 255))
 
-   # Draw a solid blue circle in the center
-   img = pygame.image.load('./resources/98c.jpg')
-   screen.blit(img, (0, 0))
+    screen.blit(player.surf, player.rect)
 
-   # Flip the display
-   pygame.display.flip()
+    # Flip the display
+    pygame.display.flip()
 
 # Done! Time to quit.
 pygame.quit()
