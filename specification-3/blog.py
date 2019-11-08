8de8a0e8b37cf1774428894b284190aa1cfadecd
Jay Mavin <b9034710@newcastle.ac.uk>
2019-11-08 18:24:03 +0000

    changed name to James Roh

--- specification-3/blog.py
+++ specification-3/blog.py
@@ -12,7 +12,7 @@ posts = [
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'James Smith',
+        'author': 'James Roh',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
