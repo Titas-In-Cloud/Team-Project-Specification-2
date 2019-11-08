619960219a7fed031278fa0cbb7a4c708ac3e6b9
b9016816 <b9016816@newcastle.ac.uk>
2019-11-08 16:05:40 +0000

    Filter functionality with image saving function and updated cli.py file

--- specification-2/cli.py
+++ specification-2/cli.py
@@ -9,6 +9,9 @@ def print_list_of_stored_pictures(args):
 def JPEG_thumbnail_creation(args):
     import thumbnail_creation
 
+def image_filters(args):
+    import image_filters
+
 def cli():
     parser = argparse.ArgumentParser(prog = "image_manipulator", usage = "%(prog)s [options]")
 
@@ -17,6 +20,7 @@ def cli():
     subparsers.add_parser("storage").set_defaults(func = image_storage)
     subparsers.add_parser("list").set_defaults(func=print_list_of_stored_pictures)
     subparsers.add_parser("thumbnail").set_defaults(func=JPEG_thumbnail_creation)
+    subparsers.add_parser("filters").set_defaults(func=image_filters)
 
     args = parser.parse_args()
 
