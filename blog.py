fa65171396e6e61555902304a57927169e285a94
Jay Mavin <b9034710@newcastle.ac.uk>
2019-11-09 17:24:09 +0000

    Adding content object

--- blog.py
+++ blog.py
@@ -23,6 +23,10 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
