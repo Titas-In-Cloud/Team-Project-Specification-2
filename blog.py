f36bfb0fec914136924eb01b36c0c1ed1bedc895
Jay Mavin <b9034710@newcastle.ac.uk>
2019-11-09 17:13:08 +0000

    setting up user profile picture database

--- blog.py
+++ blog.py
@@ -11,6 +11,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
 
 posts = [
     {
