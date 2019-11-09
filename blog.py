50a56d5e7b4ad5031e140b1e4ddbc3dbd85a0318
Jay Mavin <b9034710@newcastle.ac.uk>
2019-11-09 17:19:29 +0000

    Adding title object to Post class

--- blog.py
+++ blog.py
@@ -18,6 +18,9 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}'"
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
 
 posts = [
     {
