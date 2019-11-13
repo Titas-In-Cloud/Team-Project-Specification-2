26c2fb0dc562cf7e5ea20e0f31331ea77276ae11
b9055504 <e.haurat1@newcastle.ac.uk>
2019-11-13 10:24:03 +0000

    Added spec1 files

---
+++ specification1/main.py
@@ -0,0 +1,14 @@
+<<<<<<< Updated upstream
+import cli as c
+
+
+print(c)
+
+if __name__ == '__main__':
+   c.cli()
+=======
+from . import cli as c
+
+if __name__ == '__main__':
+    c.cli()
+>>>>>>> Stashed changes
